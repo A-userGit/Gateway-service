03b154041dbaa1d94b44d44346805eab770d4ec3
asemchenok <asemchenok@gmail.com>
2025-10-16 15:47:27 +0300

    initial commit

---
+++ src/main/java/com/innowise/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,10 @@
+package com.innowise.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class GatewayServiceApplication
+{
+    public static void main( String[] args ) {
+      SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
